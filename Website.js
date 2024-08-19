e85373a5262d8c4e132e73136b726249532ad859
adutb <adut.beny1@gmail.com>
2024-08-18 20:26:52 -0600

    Javascript code to make the icon interactive. Work in progress might make my own icon.

--- Website.js
+++ Website.js
@@ -1,2 +1,12 @@
-x = 5
-y = 6
+let bar = document.getElementById("options");
+bar.style.maxHeight = "0px";
+
+function toggleBar() {
+    if (bar.style.maxHeight == "0px"){
+        bar.style.maxHeight = "300px"
+    }
+    else{
+        bar.style.maxHeight = "0px"
+    }
+}
+
