8c81b6e6564c48a0902246b851a903cfcae1c977
adutb <adut.beny1@gmail.com>
2024-08-30 23:34:36 -0600

    Attempting to animate the transition with an IntersectionObserver. Needs work and maybe an HTML/CSS rewrite

--- Website.js
+++ Website.js
@@ -10,4 +10,24 @@ function toggleBar() {
     }
 }
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    })
+})
 
+const header = document.getElementsByClassName(".Header");
+const about_me_section = document.getElementById("About-Me-Section");
+const projects_section = document.getElementById("Projects-Section");
+const contacts_section = document.getElementById("Contact Section");
+
+observer.observe(header);
+observer.observe(about_me_section);
+observer.observe(projects_section);
+observer.observe(contacts_section);
