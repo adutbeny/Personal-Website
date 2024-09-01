e3077716c8b3fa8fa933a8b1687fe62b67d571aa
adutb <adut.beny1@gmail.com>
2024-08-31 18:43:38 -0600

    Intersection Observers now working within the website.

--- Website.js
+++ Website.js
@@ -1,28 +1,23 @@
-//Navigation bar code
+
 let NavList = document.querySelector(".options");
 NavList.style.maxHeight = "0px";
 function toggleBar() {
-    if (NavList.style.maxHeight == "0px"){
+    if (NavList.style.maxHeight === "0px"){
         NavList.style.maxHeight = "300px"
+
     }
     else{
         NavList.style.maxHeight = "0px"
     }
 }
+const options = {
+    rootMargin:"0px",
+    threshold: 1
+}
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show');
-        }
-        else{
-            entry.target.classList.remove('show');
-        }
-    })
-})
+const observer = new IntersectionObserver(callbackFunction, options)
 
-const header = document.getElementsByClassName(".Header");
+const header = document.getElementById("About-Me-Header");
 const about_me_section = document.getElementById("About-Me-Section");
 const projects_section = document.getElementById("Projects-Section");
 const contacts_section = document.getElementById("Contact Section");
@@ -31,3 +26,11 @@ observer.observe(header);
 observer.observe(about_me_section);
 observer.observe(projects_section);
 observer.observe(contacts_section);
+
+
+function callbackFunction(entries){
+    console.log(entries);
+    entries.forEach(entry => {
+        console.log(entry.target);
+    })
+}
